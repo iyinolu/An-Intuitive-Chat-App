a5aedf1dd296c2a496731e920d851b991cf200e9
Paul Osinowo <47259302+iyinolu@users.noreply.github.com>
2021-03-18 16:20:04 +0100

    organize directory

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+modeule.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }     
+            }
+        ]
+    }
+}
